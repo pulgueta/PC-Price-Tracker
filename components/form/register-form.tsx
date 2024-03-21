48b2fd6746f30b61543db92082e064ce527461e6
pulgueta <retardix456@gmail.com>
2024-03-21 07:26:05 -0500

    feat: add register form (no implementation yet)

---
+++ components/form/register-form.tsx
@@ -0,0 +1,37 @@
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+
+export const RegisterForm = () => {
+	return (
+		<form>
+			<h1 className='mb-2 border-b pb-2 text-2xl font-bold tracking-tight'>
+				Crear cuenta
+			</h1>
+			<Label htmlFor='email'>Correo electrónico</Label>
+			<Input
+				type='email'
+				placeholder='Tu correo electrónico'
+				className='my-2'
+				name='email'
+			/>
+			<Label htmlFor='password'>Contraseña</Label>
+			<Input
+				type='password'
+				placeholder='Tu contraseña'
+				className='my-2'
+				name='password'
+			/>
+			<Label htmlFor='confirmPassword'>Confirma tu contraseña</Label>
+			<Input
+				type='password'
+				placeholder='Confirmar contraseña'
+				className='mt-2'
+				name='confirmPassword'
+			/>
+			<Button type='submit' className='my-4 w-full'>
+				Registrarme
+			</Button>
+		</form>
+	);
+};
