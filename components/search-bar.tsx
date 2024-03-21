18ec63952b7cedf74245a86e2479d68694b0e66c
pulgueta <retardix456@gmail.com>
2024-03-21 08:27:19 -0500

    feat: add search feature to input

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import type { ElementRef, FormEvent } from 'react';
+
+import { useRouter } from 'next/navigation';
+
 import { SearchIcon } from 'lucide-react';
 
 import { useMediaQuery } from '@/hooks/use-media-query';
@@ -7,10 +11,24 @@ import { Button } from './ui/button';
 import { Input } from './ui/input';
 
 export const SearchBar = () => {
+	const { push } = useRouter();
+
 	const mediumScreen = useMediaQuery('(max-width: 990px)');
 
+	const onSubmit = (e: FormEvent<ElementRef<'form'>>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+
+		const q = formData.get('q') as string;
+
+		if (q.length < 3) return;
+
+		push(`/products?q=${q}`);
+	};
+
 	return (
-		<form className='flex items-center gap-x-2'>
+		<form className='flex items-center gap-x-2' onSubmit={onSubmit}>
 			<Input
 				className={!mediumScreen ? 'w-64' : 'w-40'}
 				name='q'
