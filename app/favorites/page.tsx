e4b37214c1a9c73154edd2fa59fe1d0db4dfa352
pulgueta <retardix456@gmail.com>
2024-03-21 08:26:33 -0500

    feat: add favorites page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,8 @@
+const Favorites = () => {
+	return (
+		<div className='flex min-h-dvh flex-col items-center justify-center gap-4 p-4 md:min-h-[calc(100vh-162px)] md:p-8 lg:min-h-[calc(100vh-160px)]'>
+			Favorites
+		</div>
+	);
+};
+export default Favorites;
