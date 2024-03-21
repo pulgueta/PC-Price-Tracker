e81b57e6a11463d914001d7ebb6e6779c9fd4ad1
pulgueta <retardix456@gmail.com>
2024-03-21 09:37:47 -0500

    feat: add drizzle config file

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
+
+import { env } from './env/server/index.mjs';
+
+export default {
+	schema: ['./db/schema.ts'],
+	out: './db/migrations',
+	driver: 'turso',
+	dbCredentials: {
+		url: env.TURSO_DATABASE_URL,
+		authToken: env.TURSO_AUTH_TOKEN,
+	},
+} satisfies Config;
