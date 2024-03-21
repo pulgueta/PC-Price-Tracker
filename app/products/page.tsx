bd37ddcf5e28ceaa34d2ddeb7881e6af65effab4
pulgueta <retardix456@gmail.com>
2024-03-21 10:56:59 -0500

    feat: add dynamic metadata based on query

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,8 @@
-import { NextPage } from 'next';
+import type { NextPage, ResolvingMetadata } from 'next';
 
 import { QueryBox } from '@/components/query-box';
 import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
 
 type $Products = {
 	searchParams: {
@@ -12,15 +13,42 @@ type $Products = {
 const Products: NextPage<$Products> = ({ searchParams }) => {
 	return (
 		<div className='flex min-h-dvh flex-col items-center gap-4 md:min-h-[calc(100vh-162px)] lg:min-h-[calc(100vh-160px)]'>
-			<h1 className='mb-2 mt-8 text-wrap text-center text-3xl font-extrabold tracking-tighter lg:text-4xl'>
+			{searchParams.q && <QueryBox query={searchParams.q} />}
+
+			<h1
+				className={cn(
+					'mb-2 text-wrap text-center text-3xl font-extrabold tracking-tighter lg:text-4xl',
+					{
+						'mt-2': searchParams.q,
+						'mt-8': !searchParams.q,
+					},
+				)}
+			>
 				{searchParams.q
 					? 'Resultados de tu búsqueda'
 					: 'Todos los productos'}
 			</h1>
-			{searchParams.q && <QueryBox query={searchParams.q} />}
 
 			<Separator className='max-w-5xl' />
 		</div>
 	);
 };
 export default Products;
+
+export async function generateMetadata(
+	{ searchParams }: $Products,
+	_parent: ResolvingMetadata,
+): Promise<ResolvingMetadata> {
+	const q = searchParams.q;
+
+	return {
+		// @ts-ignore
+		title: q ? `Búsqueda de producto: ${q}` : 'Búsqueda de productos',
+		// @ts-ignore
+		openGraph: {
+			images: [`/api/og?q=${q}`],
+		},
+		description:
+			'Busca el producto que necesites y encuentra su mejor precio en segundos.',
+	};
+}
