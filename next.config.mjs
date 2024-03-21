7b9a82210e543290d89a1a244370617c07262a68
pulgueta <retardix456@gmail.com>
2024-03-21 09:36:47 -0500

    feat: add env variables to nextjs config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 import { next } from 'million/compiler'
 
+import './env/server/index.mjs'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    poweredByHeader: false,
-    reactStrictMode: true,
-    logging: {
-        fetches: {
-            fullUrl: true,
-        }
-    },
-    experimental: { typedRoutes: true }
+	poweredByHeader: false,
+	reactStrictMode: true,
+	logging: {
+		fetches: {
+			fullUrl: true,
+		}
+	},
+	experimental: { typedRoutes: true }
 };
 
 export default next(nextConfig);
