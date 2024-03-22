6fac24a177fcdcef058952040961b83c9d8e98fd
pulgueta <retardix456@gmail.com>
2024-03-22 00:34:34 -0500

    fix: remove encryption key

--- db/index.ts
+++ db/index.ts
@@ -6,7 +6,6 @@ import { env } from '@/env/server/index.mjs';
 const turso = createClient({
 	url: env.TURSO_DATABASE_URL,
 	authToken: env.TURSO_AUTH_TOKEN,
-	encryptionKey: env.TURSO_ENCRYPTION_KEY,
 });
 
 export const db = drizzle(turso);
