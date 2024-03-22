d43db850185d78d47b583f7babab5b3169eebf91
pulgueta <retardix456@gmail.com>
2024-03-22 00:35:04 -0500

    style: create custom submit button

---
+++ components/form/submit-button.tsx
@@ -0,0 +1,15 @@
+import { useFormStatus } from 'react-dom';
+
+import { Loader2Icon } from 'lucide-react';
+
+import { Button } from '../ui/button';
+
+export const SubmitButton = ({ label }: { label: string }) => {
+	const { pending } = useFormStatus();
+
+	return (
+		<Button type='submit' className='my-4 w-full' disabled={pending}>
+			{pending ? <Loader2Icon className='size-4 animate-spin' /> : label}
+		</Button>
+	);
+};
