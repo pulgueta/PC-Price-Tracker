a0f0ae70687d15ec018f6b0611d911d62ff08e95
pulgueta <retardix456@gmail.com>
2024-03-22 14:14:49 -0500

    refactor: remove client directive and added custom separator

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,25 +1,18 @@
-'use client';
-
 import { LoginForm } from '@/components/form/login-form';
 import { RegisterForm } from '@/components/form/register-form';
-import { Separator } from '@/components/ui/separator';
-import { useMediaQuery } from '@/hooks/use-media-query';
 
 const Auth = () => {
-	const mediumScreen = useMediaQuery('(max-width: 990px)');
-
 	return (
-		<div className='flex min-h-dvh flex-col items-center justify-center gap-4 p-4 md:min-h-[calc(100vh-162px)] md:p-8 lg:min-h-[calc(100vh-160px)]'>
+		<section className='flex min-h-dvh flex-col items-center justify-center gap-4 p-4 md:min-h-[calc(100vh-162px)] md:p-8 lg:min-h-[calc(100vh-160px)]'>
 			<article className='flex w-full flex-col justify-center gap-8 rounded border p-8 shadow lg:w-auto lg:flex-row'>
 				<LoginForm />
-				{mediumScreen ? (
-					<Separator orientation='horizontal' />
-				) : (
-					<div aria-hidden className='mx-4 w-1 border' />
-				)}
+				<div
+					className='h-1 w-full rounded border lg:mx-4 lg:h-96 lg:w-1'
+					aria-hidden
+				/>
 				<RegisterForm />
 			</article>
-		</div>
+		</section>
 	);
 };
 export default Auth;
