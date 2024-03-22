f0992a4f68071f56b3bdf776df424425df06d38c
pulgueta <retardix456@gmail.com>
2024-03-22 00:38:24 -0500

    refactor: update file name

---
+++ schemas/registerSchema.ts
@@ -0,0 +1,18 @@
+import { object, string } from 'zod';
+
+export const registerSchema = object({
+	email: string()
+		.min(6, 'El correo electrónico debe tener al menos 6 caracteres.')
+		.email({ message: 'Debes ingresar un correo electrónico válido' }),
+	password: string().min(
+		6,
+		'La contraseña debe ser de al menos 6 caracteres.',
+	),
+	confirmPassword: string().min(
+		6,
+		'La contraseña debe ser de al menos 6 caracteres.',
+	),
+}).refine(({ password, confirmPassword }) => password === confirmPassword, {
+	message: 'Las contraseñas deben coincidir',
+	path: ['confirmPassword'],
+});
