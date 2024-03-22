10a7e1c18e73b408450a7d1e0ba1bbf387763a01
pulgueta <retardix456@gmail.com>
2024-03-22 14:15:22 -0500

    feat: add message to screen to let the user know the account was created and needs to login

--- components/form/register-form.tsx
+++ components/form/register-form.tsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from 'react';
+'use client';
+
+import { type ElementRef, useEffect, useRef, useState } from 'react';
 import { useFormState } from 'react-dom';
+
+import { useRouter } from 'next/navigation';
+
+import { CheckIcon, InfoIcon } from 'lucide-react';
 
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 import { SubmitButton } from './submit-button';
 import { register } from '@/actions/auth/register';
+import { cn } from '@/lib/utils';
 
 export const RegisterForm = () => {
-	const [state, formAction] = useFormState(register, undefined);
 	const [show, setShow] = useState(false);
+	const form = useRef<ElementRef<'form'>>(null);
+	const [state, formAction] = useFormState(register, undefined);
+
+	const { refresh } = useRouter();
 
 	useEffect(() => {
-		if (state) {
+		if (state?.success) {
 			setShow(true);
+			form.current?.reset();
+			refresh();
 		}
-	}, []);
+	}, [state]);
 
 	return (
-		<form action={formAction}>
+		<form action={formAction} ref={form}>
 			<h1 className='mb-2 border-b pb-2 text-2xl font-bold tracking-tight'>
 				Crear cuenta
 			</h1>
@@ -45,7 +57,25 @@ export const RegisterForm = () => {
 				name='confirmPassword'
 				autoComplete='confirmPassword'
 			/>
-			{show && <span>{state?.message}</span>}
+			{show && (
+				<span
+					className={cn(
+						'my-4 flex items-center gap-x-2 text-pretty rounded p-4 text-sm',
+						{
+							'bg-green-200 text-green-800': state?.success,
+							'bg-red-200 text-red-800': !state?.success,
+						},
+					)}
+				>
+					{state?.success ? (
+						<CheckIcon className='size-6 md:size-4' aria-hidden />
+					) : (
+						<InfoIcon className='size-6 md:size-4' aria-hidden />
+					)}{' '}
+					{state?.message}
+				</span>
+			)}
+
 			<SubmitButton label='Crear mi cuenta' />
 		</form>
 	);
