be7e2162efe28e6eed9d8834395f70584271100b
pulgueta <retardix456@gmail.com>
2024-03-22 00:39:38 -0500

    feat: use react hooks for form handling, not working yet

--- components/form/register-form.tsx
+++ components/form/register-form.tsx
@@ -1,10 +1,23 @@
-import { Button } from '../ui/button';
+import { useEffect, useState } from 'react';
+import { useFormState } from 'react-dom';
+
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
+import { SubmitButton } from './submit-button';
+import { register } from '@/actions/auth/register';
 
 export const RegisterForm = () => {
+	const [state, formAction] = useFormState(register, undefined);
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		if (state) {
+			setShow(true);
+		}
+	}, []);
+
 	return (
-		<form>
+		<form action={formAction}>
 			<h1 className='mb-2 border-b pb-2 text-2xl font-bold tracking-tight'>
 				Crear cuenta
 			</h1>
@@ -14,6 +27,7 @@ export const RegisterForm = () => {
 				placeholder='Tu correo electrónico'
 				className='my-2'
 				name='email'
+				autoComplete='email'
 			/>
 			<Label htmlFor='password'>Contraseña</Label>
 			<Input
@@ -21,6 +35,7 @@ export const RegisterForm = () => {
 				placeholder='Tu contraseña'
 				className='my-2'
 				name='password'
+				autoComplete='password'
 			/>
 			<Label htmlFor='confirmPassword'>Confirma tu contraseña</Label>
 			<Input
@@ -28,10 +43,10 @@ export const RegisterForm = () => {
 				placeholder='Confirmar contraseña'
 				className='mt-2'
 				name='confirmPassword'
+				autoComplete='confirmPassword'
 			/>
-			<Button type='submit' className='my-4 w-full'>
-				Registrarme
-			</Button>
+			{show && <span>{state?.message}</span>}
+			<SubmitButton label='Crear mi cuenta' />
 		</form>
 	);
 };
