b943f3a2fdd739734ece20fca6f36568e84e888f
pulgueta <retardix456@gmail.com>
2024-03-22 13:52:47 -0500

    refactor: remove console.log and changed success message

--- actions/auth/register.ts
+++ actions/auth/register.ts
@@ -11,7 +11,6 @@ export const register = async (
 	const values = registerSchema.safeParse(Object.fromEntries(e.entries()));
 
 	if (!values.success) {
-		console.log(values.error.flatten().fieldErrors);
 		return {
 			success: false,
 			message: 'Verifica los datos ingresados',
@@ -26,10 +25,10 @@ export const register = async (
 		return { success: false, message: 'El correo ya está siendo usado.' };
 	}
 
-	const user = await createUser(values.data);
+	await createUser(values.data);
 
 	return {
 		success: true,
-		message: `El usuario con correo ${user.email}`,
+		message: 'Cuenta creada, ahora debes iniciar sesión.',
 	};
 };
