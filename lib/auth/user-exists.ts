66d6cb84da0974c059951272fbbd0206348ef8c4
pulgueta <retardix456@gmail.com>
2024-03-22 13:51:02 -0500

    docs: add JSDoc comment for future reference

--- lib/auth/user-exists.ts
+++ lib/auth/user-exists.ts
@@ -3,7 +3,12 @@ import { eq } from 'drizzle-orm';
 import { db } from '@/db';
 import { userSchema } from '@/db/schemas/user';
 
-export const userExistsWithEmail = async (email: string) => {
+/**
+ * Verifies if user exists with a given email
+ * @param {string} email - The email to check if exists in database.
+ * @returns {Promise<boolean>} true or false
+ */
+export const userExistsWithEmail = async (email: string): Promise<boolean> => {
 	const [userExists] = await db
 		.select()
 		.from(userSchema)
