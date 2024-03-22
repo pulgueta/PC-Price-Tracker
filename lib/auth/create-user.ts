241328f81094fccd749a9567948ab0561b7e40af
pulgueta <retardix456@gmail.com>
2024-03-22 13:53:17 -0500

    feat: add argon2 for password encryption and handle values inserted separately

--- lib/auth/create-user.ts
+++ lib/auth/create-user.ts
@@ -1,11 +1,21 @@
+import { hash } from 'argon2';
+
 import { db } from '@/db';
 import { UserSchema, userSchema } from '@/db/schemas/user';
 
-export const createUser = async (data: UserSchema) => {
+/**
+ * Creates a new user in SQLite
+ * @param {object} data - User data
+ * @returns {Promise<string>} User email
+ */
+export const createUser = async (data: UserSchema): Promise<string> => {
 	const [user] = await db
 		.insert(userSchema)
-		.values(data)
+		.values({
+			email: data.email,
+			password: await hash(data.password),
+		})
 		.returning({ email: userSchema.email });
 
-	return user;
+	return user.email;
 };
