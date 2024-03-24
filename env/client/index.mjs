563c57003c7d34b8eea38b6e470874ab6146bc6f
pulgueta <retardix456@gmail.com>
2024-03-24 08:09:52 -0500

    feat: add sentry integration

--- env/client/index.mjs
+++ env/client/index.mjs
@@ -3,11 +3,12 @@ import { ZodError, string } from 'zod';
 
 export const env = createEnv({
 	client: {
-		NEXT_PUBLIC_ADSENSE_ID: string().min(8)
+		NEXT_PUBLIC_ADSENSE_ID: string().min(8),
+		NEXT_PUBLIC_SENTRY_DSN: string().url(),
 	},
 	runtimeEnv: {
 		NEXT_PUBLIC_ADSENSE_ID: process.env.NEXT_PUBLIC_ADSENSE_ID,
-
+		NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
 	},
 	onValidationError: (error = ZodError) => {
 		throw new Error('[*] Invalid environment variables', error.flatten().fieldErrors);
--- env/server/index.mjs
+++ env/server/index.mjs
@@ -5,10 +5,12 @@ export const env = createEnv({
 	server: {
 		TURSO_DATABASE_URL: string().url(),
 		TURSO_AUTH_TOKEN: string().min(32),
+		SENTRY_DSN: string().url()
 	},
 	runtimeEnv: {
 		TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
 		TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+		SENTRY_DSN: process.env.SENTRY_DSN
 	},
 	onValidationError: (error = ZodError) => {
 		throw new Error('[*] Invalid environment variables', error.flatten().fieldErrors);
