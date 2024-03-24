dfd3f3f54189b150d337c5898d7c65a7c3877378
pulgueta <retardix456@gmail.com>
2024-03-24 07:53:32 -0500

    feat: add function to get user by email independent on ORM

--- lib/auth/user-exists.ts
+++ lib/auth/user-exists.ts
@@ -1,7 +1,7 @@
 import { eq } from 'drizzle-orm';
 
 import { db } from '@/db';
-import { userSchema } from '@/db/schemas/user';
+import { UserSchema, userSchema } from '@/db/schemas/user';
 
 /**
  * Verifies if user exists with a given email
@@ -15,4 +15,18 @@ export const userExistsWithEmail = async (email: string): Promise<boolean> => {
 		.where(eq(userSchema.email, email));
 
 	return userExists ? true : false;
+};
+
+/**
+ * Verifies and returns user data with a given email.
+ * @param {string} email - The email to check if exists in database.
+ * @returns {Promise<boolean>} true or false
+ */
+export const getUserByEmail = async (email: string): Promise<UserSchema> => {
+	const [userExists] = await db
+		.select()
+		.from(userSchema)
+		.where(eq(userSchema.email, email));
+
+	return userExists;
 };
