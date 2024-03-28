b86c7adc4aa2d4571d9c242c7947a1e38b7bd147
pulgueta <retardix456@gmail.com>
2024-03-27 21:40:30 -0500

    feat: add random string generator for analytics

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const randomString = (length: number) => {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_';
+	const charactersLength = characters.length;
+
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength),
+		);
+	}
+
+	return result;
+};
