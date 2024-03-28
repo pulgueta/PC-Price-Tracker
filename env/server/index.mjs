d51b13af9a4bd84374ccf4f723af6c1032ae854c
pulgueta <retardix456@gmail.com>
2024-03-27 21:41:07 -0500

    feat: add recaptcha env variables

--- env/server/index.mjs
+++ env/server/index.mjs
@@ -6,11 +6,13 @@ export const env = createEnv({
 		TURSO_DATABASE_URL: string().url(),
 		TURSO_AUTH_TOKEN: string().min(32),
 		SENTRY_DSN: string().url(),
+		RECAPTCHA_KEY: string().min(8),
 	},
 	runtimeEnv: {
 		TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
 		TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
 		SENTRY_DSN: process.env.SENTRY_DSN,
+		RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
 	},
 	onValidationError: (error = ZodError) => {
 		throw new Error(
