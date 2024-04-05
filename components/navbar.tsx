1b3dc80cca583251507566197da1cf051dc2277c
pulgueta <retardix456@gmail.com>
2024-04-05 18:07:01 -0500

    feat: remove unuseful hook

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 import Link from 'next/link';
 
 import { MenuIcon, UserIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
-import { useMediaQuery } from '@/hooks/use-media-query';
+
 import { buttonVariants } from './ui/button';
 import {
 	DropdownMenu,
@@ -15,9 +13,10 @@ import {
 	DropdownMenuTrigger,
 } from './ui/dropdown-menu';
 import { SearchBar } from './search-bar';
+import { getSession } from '@/lib/auth/get-session';
 
-export const Navbar = () => {
-	const mediumScreen = useMediaQuery('(max-width: 990px)');
+export const Navbar = async () => {
+	const session = await getSession();
 
 	return (
 		<nav className='flex items-center justify-between border-y p-4 shadow md:justify-around'>
@@ -29,64 +28,49 @@ export const Navbar = () => {
 			</Link>
 
 			<div className='flex max-w-xs items-center space-x-2 md:max-w-lg'>
-				{mediumScreen ? (
-					<>
-						<DropdownMenu>
-							<DropdownMenuTrigger className='rounded border p-2'>
-								<MenuIcon aria-label='Menú móvil' />
-							</DropdownMenuTrigger>
-							<DropdownMenuContent>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem>
-									<SearchBar />
-								</DropdownMenuItem>
-								<DropdownMenuItem
-									className='cursor-pointer'
-									asChild
-								>
-									<Link href='/'>Inicio</Link>
-								</DropdownMenuItem>
-								<DropdownMenuItem
-									className='cursor-pointer'
-									asChild
-								>
-									<Link href='/products'>Ver productos</Link>
-								</DropdownMenuItem>
-								<DropdownMenuItem
-									className='cursor-pointer'
-									asChild
-								>
-									<Link href='/favorites'>Favoritos</Link>
-								</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
-						<Link
-							href='/auth'
-							className={buttonVariants({ size: 'icon' })}
-						>
-							<UserIcon
-								aria-label='Iniciar sesión'
-								className={cn('size-4', {
-									'mr-2': !mediumScreen,
-								})}
-							/>
-							{!mediumScreen && 'Iniciar sesión'}
-						</Link>
-					</>
-				) : (
-					<div className='flex items-center gap-x-6'>
-						<Link href='/products'>Ver productos</Link>
-						<Link href='/favorites'>Favoritos</Link>
-						<Link href='/auth' className={buttonVariants()}>
-							<UserIcon
-								className={cn('size-4', {
-									'mr-2': !mediumScreen,
-								})}
-							/>
-							{!mediumScreen && 'Iniciar sesión'}
-						</Link>
-					</div>
-				)}
+				<DropdownMenu>
+					<DropdownMenuTrigger className='block rounded border p-2 md:hidden'>
+						<MenuIcon aria-label='Menú móvil' />
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<SearchBar />
+						</DropdownMenuItem>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href='/'>Inicio</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href='/products'>Ver productos</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem className='cursor-pointer' asChild>
+							<Link href='/favorites'>Favoritos</Link>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+				<Link
+					href={session ? '/' : '/auth'}
+					className={buttonVariants({
+						size: 'icon',
+						className: 'block md:hidden',
+					})}
+				>
+					<UserIcon
+						aria-label='Iniciar sesión'
+						className='size-4 md:size-8'
+					/>
+				</Link>
+
+				<div className='hidden items-center gap-x-6 md:flex'>
+					<Link href='/products'>Ver productos</Link>
+					<Link href='/favorites'>Favoritos</Link>
+					<Link
+						href={session ? '/' : '/auth'}
+						className={buttonVariants()}
+					>
+						<UserIcon className={cn('size-4')} />
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);
