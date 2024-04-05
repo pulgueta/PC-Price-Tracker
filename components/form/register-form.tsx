26e00d5b028b0795b6b6726a83d7c7d15b48bb8d
pulgueta <retardix456@gmail.com>
2024-04-05 18:08:25 -0500

    feat: remove useFormState and moving to shadcn form component

--- components/form/register-form.tsx
+++ components/form/register-form.tsx
@@ -1,33 +1,17 @@
 'use client';
 
-import {
-	type ElementRef,
-	type FormEvent,
-	useEffect,
-	useRef,
-	useState,
-} from 'react';
-import { useFormState } from 'react-dom';
-
-import { useRouter } from 'next/navigation';
+import type { FormEvent } from 'react';
 
 import { toast } from 'sonner';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
-import { CheckIcon, InfoIcon } from 'lucide-react';
 
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 import { SubmitButton } from './submit-button';
-import { register } from '@/actions/auth/register';
-import { cn, randomString } from '@/lib/utils';
+
+import { randomString } from '@/lib/utils';
 
 export const RegisterForm = () => {
-	const [show, setShow] = useState(false);
-	const form = useRef<ElementRef<'form'>>(null);
-	const [state, formAction] = useFormState(register, undefined);
-
-	const { refresh } = useRouter();
-
 	const { executeRecaptcha } = useGoogleReCaptcha();
 
 	const onSubmit = async (e: FormEvent) => {
@@ -61,16 +45,8 @@ export const RegisterForm = () => {
 		}
 	};
 
-	useEffect(() => {
-		if (state?.success) {
-			setShow(true);
-			form.current?.reset();
-			refresh();
-		}
-	}, [refresh, state]);
-
 	return (
-		<form action={formAction} onSubmit={onSubmit} ref={form}>
+		<form onSubmit={onSubmit}>
 			<h1 className='mb-2 border-b pb-2 text-2xl font-bold tracking-tight'>
 				Crear cuenta
 			</h1>
@@ -98,24 +74,6 @@ export const RegisterForm = () => {
 				name='confirmPassword'
 				autoComplete='confirmPassword'
 			/>
-			{show && (
-				<span
-					className={cn(
-						'my-4 flex items-center gap-x-2 text-pretty rounded p-4 text-sm',
-						{
-							'bg-green-200 text-green-800': state?.success,
-							'bg-red-200 text-red-800': !state?.success,
-						},
-					)}
-				>
-					{state?.success ? (
-						<CheckIcon className='size-6 md:size-4' aria-hidden />
-					) : (
-						<InfoIcon className='size-6 md:size-4' aria-hidden />
-					)}{' '}
-					{state?.message}
-				</span>
-			)}
 
 			<SubmitButton label='Crear mi cuenta' />
 		</form>
