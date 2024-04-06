50ef0370a757de8590c22e304b575ecca8370b17
pulgueta <retardix456@gmail.com>
2024-04-06 16:16:29 -0500

    feat: add AUTH_SECRET environment variable for authentication

--- env/server/index.mjs
+++ env/server/index.mjs
@@ -7,12 +7,14 @@ export const env = createEnv({
 		TURSO_AUTH_TOKEN: string().min(32),
 		SENTRY_DSN: string().url(),
 		RECAPTCHA_KEY: string().min(8),
+		AUTH_SECRET: string().min(32),
 	},
 	runtimeEnv: {
 		TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
 		TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
 		SENTRY_DSN: process.env.SENTRY_DSN,
 		RECAPTCHA_KEY: process.env.RECAPTCHA_KEY,
+		AUTH_SECRET: process.env.AUTH_SECRET,
 	},
 	onValidationError: (error = ZodError) => {
 		throw new Error(
