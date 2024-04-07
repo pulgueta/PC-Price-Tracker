e783e856ab21b6eea44f7cab2c091f6868af2261
pulgueta <retardix456@gmail.com>
2024-04-06 19:26:45 -0500

    feat: moved from server-action-based form to shadcn-powered form component

--- components/form/register-form.tsx
+++ components/form/register-form.tsx
@@ -1,22 +1,36 @@
 'use client';
 
-import type { FormEvent } from 'react';
-
 import { toast } from 'sonner';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Input } from '../ui/input';
-import { Label } from '../ui/label';
-import { SubmitButton } from './submit-button';
-
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '../ui/form';
+import { Button } from '../ui/button';
 import { randomString } from '@/lib/utils';
+import { type Register, registerSchema } from '@/schemas/registerSchema';
 
 export const RegisterForm = () => {
+	const form = useForm<Register>({
+		resolver: zodResolver(registerSchema),
+		defaultValues: {
+			email: '',
+			confirmPassword: '',
+			password: '',
+		},
+	});
+
 	const { executeRecaptcha } = useGoogleReCaptcha();
 
-	const onSubmit = async (e: FormEvent) => {
-		e.preventDefault();
-
+	const onSubmit = form.handleSubmit(async (formData) => {
 		if (!executeRecaptcha) {
 			toast.error('ReCAPTCHA', {
 				description: 'Error al validar el captcha.',
@@ -42,40 +56,98 @@ export const RegisterForm = () => {
 			toast.error('ReCAPTCHA', {
 				description: 'Presunto bot detectado.',
 			});
+			return;
 		}
-	};
+
+		const register = await fetch('/api/register', {
+			method: 'POST',
+			body: JSON.stringify(formData),
+		});
+
+		const regData = await register.json();
+
+		if (!register.ok) {
+			toast.error(data.error);
+			return;
+		}
+
+		toast.success('Cuenta creada', {
+			description: regData.message,
+		});
+
+		form.reset();
+		form.clearErrors();
+	});
 
 	return (
-		<form onSubmit={onSubmit}>
+		<div className='flex flex-col gap-2'>
 			<h1 className='mb-2 border-b pb-2 text-2xl font-bold tracking-tight'>
 				Crear cuenta
 			</h1>
-			<Label htmlFor='email'>Correo electrónico</Label>
-			<Input
-				type='email'
-				placeholder='Tu correo electrónico'
-				className='my-2'
-				name='email'
-				autoComplete='email'
-			/>
-			<Label htmlFor='password'>Contraseña</Label>
-			<Input
-				type='password'
-				placeholder='Tu contraseña'
-				className='my-2'
-				name='password'
-				autoComplete='password'
-			/>
-			<Label htmlFor='confirmPassword'>Confirma tu contraseña</Label>
-			<Input
-				type='password'
-				placeholder='Confirmar contraseña'
-				className='mt-2'
-				name='confirmPassword'
-				autoComplete='confirmPassword'
-			/>
+			<Form {...form}>
+				<form onSubmit={onSubmit} className='space-y-4'>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Correo electrónico</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Tu correo electrónico'
+										type='email'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Contraseña</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Tu contraseña'
+										type='password'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='confirmPassword'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Confirma tu contraseña</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='Tu contraseña'
+										type='password'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-			<SubmitButton label='Crear mi cuenta' />
-		</form>
+					<Button
+						disabled={form.formState.isSubmitting}
+						className='w-full'
+					>
+						{form.formState.isSubmitting
+							? 'Creando cuenta'
+							: 'Crear cuenta'}
+					</Button>
+				</form>
+			</Form>
+		</div>
 	);
 };
