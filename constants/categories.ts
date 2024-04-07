458af32f6e8accb6556ee367268057c151e27a20
pulgueta <retardix456@gmail.com>
2024-04-06 19:25:14 -0500

    feat: add categories array with initial products to be searched

---
+++ constants/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+	{ label: 'Procesadores', value: 'cpu' },
+	{ label: 'Tarjetas gráficas', value: 'gpu' },
+	{ label: 'Fuentes de poder', value: 'psu' },
+	{ label: 'Cajas', value: 'case' },
+	{ label: 'Memoria RAM', value: 'ram' },
+	{ label: 'Placas base', value: 'mobo' },
+] as const;
