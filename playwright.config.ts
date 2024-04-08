6efbc8503d8fbae0655b5275795f65b2d1309c40
pulgueta <retardix456@gmail.com>
2024-04-08 18:29:05 -0500

    test: modify playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,22 +3,14 @@ import 'dotenv/config';
 
 export default defineConfig({
 	testDir: './tests',
-	/* Run tests in files in parallel */
 	fullyParallel: true,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://127.0.0.1:3000',
-
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		baseURL: 'http://127.0.0.1:3000',
+		screenshot: 'only-on-failure',
 		trace: 'on-first-retry',
 	},
 	projects: [
@@ -45,11 +37,9 @@ export default defineConfig({
 			use: { ...devices['iPhone 12'] },
 		},
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: 'pnpm start',
+		url: 'http://127.0.0.1:3000',
+		reuseExistingServer: !process.env.CI,
+	},
 });
