5ee27252df1ef258ab33b55845bb05e81f6ddd36
pulgueta <retardix456@gmail.com>
2024-04-09 00:58:03 -0500

    test: split test in 2 for checking initial content and make a simple query

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -1,25 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+import { landing, buttons, categories, placeholders } from '@/i18n/es.json';
+
 test('Checks initial content', async ({ page }) => {
 	await page.goto('/');
 
-	const url = 'http://127.0.0.1:3000/';
-	const query = 'nvidia rtx';
+	const url = process.env.BASE_URL as string;
 
 	expect(page.url()).toBe(url);
 
 	const h1 = page.getByRole('heading', {
-		name: 'Todos los precios de tus componentes favoritos',
+		name: landing.heading,
 		exact: true,
 	});
 
 	const h2 = page.getByRole('heading', {
-		name: '¡En un solo lugar!',
+		name: landing.subheading,
 		exact: true,
 	});
 
 	const h3 = page.getByRole('heading', {
-		name: 'Aquí encontrarás precios de:',
+		name: landing.storesHeading,
 		exact: true,
 	});
 
@@ -27,16 +28,38 @@ test('Checks initial content', async ({ page }) => {
 	expect(h2).toBeVisible();
 	expect(h3).toBeVisible();
 
-	await page.getByPlaceholder('Intel Core i7...').fill(query);
+	expect(
+		page.getByPlaceholder(placeholders.inputs.queryPlaceholder),
+	).toBeVisible();
 
-	await page.getByText('Selecciona una categoría').click();
-	await page.getByRole('option', { name: 'Tarjetas gráficas' }).click();
+	expect(page.getByText(buttons.combobox)).toBeVisible();
+});
 
-	await page.getByRole('button', { name: 'Buscar' }).click();
+test('Inserts a query and goes to products page', async ({ page }) => {
+	await page.goto('/');
 
-	await page.waitForURL('**/products');
+	const url = process.env.BASE_URL as string;
+	const query = 'nvidia rtx';
+	const encodedQuery = encodeURIComponent(query);
 
-	console.log(page.url());
+	expect(page.url()).toBe(url);
 
-	// expect(page.url()).toBe(`${url}products?q=${query.replace(' ', '-')}`);
+	await page
+		.getByPlaceholder(placeholders.inputs.queryPlaceholder)
+		.fill(query);
+
+	await page.getByText(buttons.combobox).click();
+	await page.getByRole('option', { name: categories.gpu.label }).click();
+
+	await page
+		.getByRole('button', { name: buttons.searchButton })
+		.click({ force: true, noWaitAfter: true });
+
+	await page.waitForURL(
+		`http://127.0.0.1:3000/products?q=${encodedQuery}&category=${categories.gpu.value}`,
+	);
+
+	expect(page.url()).toBe(
+		`${url}products?q=${encodedQuery}&category=${categories.gpu.value}`,
+	);
 });
