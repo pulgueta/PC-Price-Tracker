ebe3751a3eca7e42a085fd4fd08f7b9f0954fb16
pulgueta <retardix456@gmail.com>
2024-04-14 03:55:26 -0500

    feat: add tanstack provider

--- providers/index.tsx
+++ providers/index.tsx
@@ -4,11 +4,14 @@ import type { FC, PropsWithChildren } from 'react';
 
 import { ProgressProvider } from './progress-bar-provider';
 import { AuthProvider } from './auth-provider';
+import { TanStackProvider } from './tanstack-provider';
 
 export const ClientComponentProviders: FC<PropsWithChildren> = ({
 	children,
 }) => (
 	<ProgressProvider>
-		<AuthProvider>{children}</AuthProvider>
+		<AuthProvider>
+			<TanStackProvider>{children}</TanStackProvider>
+		</AuthProvider>
 	</ProgressProvider>
 );
